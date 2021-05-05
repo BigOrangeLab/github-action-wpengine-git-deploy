85dc7b9e9824a45d7ac525cecb920282e4e6d9b6
Eduard Milushi <emilushi@gmail.com>
2021-05-05 22:41:26 +0200

    added npm build

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,8 +29,8 @@ chmod 600 "$WPENGINE_SSH_KEY_PRIVATE_PATH"
 chmod 644 "$WPENGINE_SSH_KEY_PUBLIC_PATH"
 
 cd wp-content/themes/bolbase
-pwd
-ls -la
+
+npm run build
 
 git config core.sshCommand "ssh -i $WPENGINE_SSH_KEY_PRIVATE_PATH -o UserKnownHostsFile=$KNOWN_HOSTS_PATH"
 git remote add $WPENGINE_ENV git@$WPENGINE_HOST:$WPENGINE_ENV/$WPENGINE_ENVIRONMENT_NAME.git
